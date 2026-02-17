796a372431f904e9b71c26914ebc40045f2f1b33
MahenderJakhar27 <m.jakhar1999@gmail.com>
2026-02-17 16:03:01 +0530

    Initial setup: Django project with DRF and JWT authentication

---
+++ config/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Register
+    path('api/', include('accounts.urls')),
+
+    # Login
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    # Refresh
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
